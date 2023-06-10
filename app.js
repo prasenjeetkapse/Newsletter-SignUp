fc5ba856ac604cdfdab3a1d8f1b00033ecbc8c80
Prasenjeet Kapse <prasenjeetkapse1@gmail.com>
2023-06-10 20:36:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+ const app = express();
+
+
+ app.use(express.static("public"));
+ app.use(bodyParser.urlencoded({extended:true}));
+
+ app.get("/", function(req,res){
+    res.sendFile(__dirname+"/signup.html")
+ });
+
+ app.post("/", function(req,res){
+    const firstName= req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    
+    
+    const data = {
+      members:[
+         {
+            email_address: email,
+            status: "subscribed",
+            merge_fields:{
+               FNAME: firstName,
+               LNAME: lastName
+            }
+         }
+      ]
+    };
+
+    var jsonData = JSON.stringify(data);
+
+    const url = 'https://us21.api.mailchimp.com/3.0/lists/75044961ba'
+    const options ={
+      method: "POST",
+      auth: "prasenjeet:f80392e6d84cfe77b6fe000a7426b1cd-us21"
+    }
+    
+    const request = https.request(url,options,function(response){  
+         if(response.statusCode==200){
+            res.sendFile(__dirname + "/success.html");
+         }
+         else{
+            res.sendFile(__dirname+"/failure.html")
+         }
+    })
+    request.write(jsonData);
+    request.end();
+ });
+
+
+app.post("/failure", function(req,res){
+   res.redirect("/");
+});
+ app.listen(process.env.PORT || 3000, function(req,res){
+    console.log("Server is ready at port 3000");
+ });
+
+// api id
+// f80392e6d84cfe77b6fe000a7426b1cd-us21   new
+//  ea3d93672b0d69fcc9f8a0d7fc6695b3-us21
+// audince id or list Id
+// 75044961ba.
